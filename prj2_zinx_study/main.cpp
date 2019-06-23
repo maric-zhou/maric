345726773ddd32a3d21ba1e550cda791a15d06fe
marklion <marklion@sina.com>
2019-06-23 12:17:41 +0800

    添加退出功能

--- prj2_zinx_study/main.cpp
+++ prj2_zinx_study/main.cpp
@@ -4,6 +4,40 @@
 using namespace std;
 /*读标准输入，回显到标准输出*/
 
+/*创建标准输出通道类*/
+class TestStdout :public Ichannel {
+	// 通过 Ichannel 继承
+	virtual bool Init() override
+	{
+		return true;
+	}
+	virtual bool ReadFd(std::string & _input) override
+	{
+		return false;
+	}
+	virtual bool WriteFd(std::string & _output) override
+	{
+		cout << _output << endl;
+		return true;
+	}
+	virtual void Fini() override
+	{
+	}
+	virtual int GetFd() override
+	{
+		return 1;
+	}
+	virtual std::string GetChannelInfo() override
+	{
+		return "stdout";
+	}
+	virtual AZinxHandler * GetInputNextStage(BytesMsg & _oInput) override
+	{
+		return nullptr;
+	}
+} *poOut = new TestStdout();
+
+
 /*2-写功能处理类*/
 class Echo :public AZinxHandler {
 
@@ -12,7 +46,7 @@ class Echo :public AZinxHandler {
 		/*回显到标准输出*/
 		GET_REF2DATA(BytesMsg, input, _oInput);
 
-		cout << input.szData << endl;
+		ZinxKernel::Zinx_SendOut(input.szData, *poOut);
 
 		return nullptr;
 	}
@@ -21,6 +55,27 @@ class Echo :public AZinxHandler {
 		return nullptr;
 	}
 } *poEcho = new Echo();
+
+class ExitFramework :public AZinxHandler {
+	// 通过 AZinxHandler 继承
+	virtual IZinxMsg * InternelHandle(IZinxMsg & _oInput) override
+	{
+		GET_REF2DATA(BytesMsg, obyte, _oInput);
+
+		if (obyte.szData == "exit")
+		{
+			ZinxKernel::Zinx_Exit();
+			return NULL;
+		}
+		/*创建交给下一个环节处理的数据*/
+		return new BytesMsg(obyte);
+	}
+	virtual AZinxHandler * GetNextHandler(IZinxMsg & _oNextMsg) override
+	{
+		return poEcho;
+	}
+} *poExit = new ExitFramework();
+
 
 /*3-写通道类*/
 class TestStdin :public Ichannel {
@@ -51,7 +106,7 @@ class TestStdin :public Ichannel {
 	}
 	virtual AZinxHandler * GetInputNextStage(BytesMsg & _oInput) override
 	{
-		return poEcho;
+		return poExit;
 	}
 };
 
@@ -63,6 +118,7 @@ int main()
 	/*4-将通道对象添加到框架*/
 	TestStdin *poStdin = new TestStdin();
 	ZinxKernel::Zinx_Add_Channel(*poStdin);
+	ZinxKernel::Zinx_Add_Channel(*poOut);
 
 	/*5-运行框架*/
 	ZinxKernel::Zinx_Run();
